9fb421ee4c63660bc2c194aa2c899d463896973a
Martin <mh866@cornell.edu>
2017-10-01 15:13:07 -0400

    Added master and slave BT communication

---
+++ comms/master.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+import bluetooth
+import sys
+import struct
+import time
+import operator
+import threading
+
+serverMACAddress = ''
+port = 1
+size = 1024
+class BTInterface_Master(object):
+
+  def __init__(self):
+  	  self.target_name = ""
+      self.port = port
+      self.device_found = False
+      self.tries = 0
+      self.target_addr = target_addr
+      self.sock = None
+
+  def connect(self):
+    if self.target_addr is None:
+
+        for i in range(10):
+          nearby_devices = bluetooth.discover_devices(lookup_names = True)
+
+          if len(nearby_devices)>0:
+            for bdaddr, name in nearby_devices:
+              if name.startswith(self.target_name):
+                self.device_found = True
+                self.target_addr = bdaddr
+                break
+          if self.device_found:
+            break
+
+    try:
+      self.sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+      self.sock.connect((self.target_address,self.port))
+    except bluetooth.btcommon.BluetoothError as error:
+      sys.stdout.write(error.strerror)
+      sys.stdout.flush()
+      time.sleep(5.0)
+      sys.exit(1)
+    sys.stdout.write("Paired with Pi @ " + self.target_addr + "\n")
+    sys.stdout.flush()
+    return True
+
+  def send(self, data):
+    self.sock.send(data)
+
+  def recv(self, num_bytes):
+    return self.sock.recv(num_bytes)
+
+  def close(self):
+    self.sock.close()
+
+if __name__ =='__main__':
+  master = BTInterface_Master()
+  master.send("Test test")
+  master.close()
---
+++ comms/slave.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import bluetooth
+import sys
+import struct
+import time
+import operator
+import threading
+
+hostMACAddress  = ''
+backlog = 1
+port  = 1
+size = 1024
+
+class BTInterface_Slave(object):
+
+  def __init__(self):
+  	  self.target_name = ""
+      self.hostMACAddress = hostMACAddress
+      self.port = port
+      self.size = size
+      self.sock = None
+
+  def listen(self):
+
+    if self.target_addr is None:
+
+        for i in range(10):
+          nearby_devices = bluetooth.discover_devices(lookup_names = True)
+
+          if len(nearby_devices)>0:
+            for bdaddr, name in nearby_devices:
+              if name.startswith(self.target_name):
+                self.device_found = True
+                self.target_addr = bdaddr
+                break
+          if self.device_found:
+            break
+
+    try:
+      self.sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+      self.sock.connect((self.hostMACAddress,self.port))
+      self.sock.listen(backlog)
+      client, clientinfo = self.sock.accept()
+      while 1:
+        data = client.recv(size)
+        if data:
+          print(data)
+
+    except bluetooth.btcommon.BluetoothError as error:
+      sys.stdout.write(error.strerror)
+      sys.stdout.flush()
+      time.sleep(5.0)
+      sys.exit(1)
+
+if __name__ =='__main__':
+  listener = BTInterface_Slave()
+  listener.listen()
