ba1a79b02422f305c356469edb343d445ee3f973
Martin <mh866@cornell.edu>
2017-10-13 00:02:58 -0400

    Added servo calibration script

--- control/crServoCtrl.py
+++ control/crServoCtrl.py
@@ -8,6 +8,23 @@ import threading
 
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM) # Use broadcom numbering
+# Set output pins - pin 26 BCM, pin 37 BOARD
 GPIO.setup(26,GPIO.OUT)
 
+# Set frequency to 70 Hz - period=20ms
+# Full Stop - High for 1.5 ms -- 7.5% duty cycle
+# Full Forward - high for 2.0 ms -- 10.0% duty cycle
+# Full Backward - high for 1.0 ms -- 5.0% duty cycle 
+p= GPIO.PWM(26,70) # Set BCM pin 26 at 70Hz
+p.start(7.5)	   # Set duty cycle to 7.5% and start
+
+# Spin for 30 seconds
+timeElapsed = time.tim()
+try: 
+        while time.time()-timeElapsed < 30:
+                pass
+        
+except KeyboardInterrupt:
+        GPIO.cleanup()
+GPIO.cleanup()
