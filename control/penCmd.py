5ad120ab079328bbb801b7034515db86c94ee43f
Martin <mh866@cornell.edu>
2017-11-01 19:30:17 -0400

    added pen control tests

---
+++ control/penCmd.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env/python
+# Authors: Martin Herrera and Deepthi Krovvidi
+
+
+import RPi.GPIO as GPIO         #import RPi.GPIO module
+import time
+import sys
+
+def penActuation(pin, dir):
+	 GPIO.setmode(GPIO.BCM)          # choose BCM
+     GPIO.setup(pin, GPIO.OUT)       # set pin as an output
+     pwm = GPIO.PWM(pin, 50)         # setting pin and frequency
+     pwm.start(0)                    # start LED on 0% duty cycle (off)
+
+     # 0 degrees is 2.72 and 180 degrees is 12
+
+     if dir == 'up':
+     	pwm.ChangeDutyCycle(2.72)
+     if dir == 'down':
+     	pwm.ChangeDutyCycle(12)
+
+     pwm.stop()
+     GPIO.cleanup()
+
+if __name__ == '__main__':
+        pin = int(sys.argv[1])
+        dir = str(sys.argv[2])
+        penActuation(pin)
--- control/penCtrlTest.py
+++ control/penCtrlTest.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env/python
-# Authors: Diego Horna (dh468), Deepthi Krovvidi (dk562)
-# Lab 3 pwm_calibrate cde
-# October 2, 2017
+#Authors: Martin Herrera and Deepthi Krovvidi 
 
 import RPi.GPIO as GPIO         #import RPi.GPIO module
 import time
@@ -9,20 +7,25 @@ import sys
 
 def RunCal(pin):
 
-	GPIO.setmode(GPIO.BCM)          # choose BCM
-	GPIO.setup(pin, GPIO.OUT)       # set pin as an output
+        GPIO.setmode(GPIO.BCM)          # choose BCM
+        GPIO.setup(pin, GPIO.OUT)       # set pin as an output
+        pwm = GPIO.PWM(pin, 50)         # setting pin and frequency
+        pwm.start(0)                    # start LED on 0% duty cycle (off)
 
-	pwm = GPIO.PWM(pin, 50)          # setting pin and frequency
-	pwm.start(0)                    # start LED on 0% duty cycle (off)
+        try:
 
-	try:
-	        while True:
-	                pwm.ChangeDutyCycle(7.5)
-
-	except KeyboardInterrupt:
-	        pwm.stop()              # stop PWM output
-	        GPIO.cleanup()          # cleanup GPIO on CTRL+C Exit
-			return
-if __name__ = '__main__':
-	pin = int(sys.argv[1])
-	RunCal(pin)
+                while True:
+                       #print('Sleeping')
+                        pwm.ChangeDutyCycle(2.72)
+                        time.sleep(1)
+                        print('Running')
+                        #pwm.ChangeDutyCycle(12)
+                        #time.sleep(1)
+        except KeyboardInterrupt:
+                pwm.ChangeDutyCycle(2.72)
+                pwm.stop()              # stop PWM output
+                GPIO.cleanup()          # cleanup GPIO on CTRL+C Exit
+                return
+if __name__ == '__main__':
+        pin = int(sys.argv[1])
+        RunCal(pin)
