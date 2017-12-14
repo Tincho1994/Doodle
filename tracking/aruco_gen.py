06f1b55c7e47320442df3d536da9c6f82d630276
Martin <mh866@cornell.edu>
2017-12-13 21:56:48 -0500

    new aruco markers added

--- tracking/aruco_gen.py
+++ tracking/aruco_gen.py
@@ -3,12 +3,12 @@ import numpy as np
 import cv2
 import cv2.aruco as aruco
  
-aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_250)
 print(aruco_dict)
 # second parameter is id number
 # last parameter is total image size
 img = aruco.drawMarker(aruco_dict, 3, 700)
-cv2.imwrite("test_marker_3.jpg", img)
+cv2.imwrite("test_markerv2_2.jpg", img)
  
 cv2.imshow('frame',img)
 cv2.waitKey(0)
