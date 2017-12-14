3ec09d2007edf563a3cc2ee676bc2f384930940c
Martin <mh866@cornell.edu>
2017-12-14 00:21:26 -0500

    new aruco tags

--- tracking/aruco_gen.py
+++ tracking/aruco_gen.py
@@ -7,8 +7,8 @@ aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_250)
 print(aruco_dict)
 # second parameter is id number
 # last parameter is total image size
-img = aruco.drawMarker(aruco_dict, 3, 700)
-cv2.imwrite("test_markerv2_2.jpg", img)
+img = aruco.drawMarker(aruco_dict, 2, 700)
+cv2.imwrite("test_markerv2_3.jpg", img)
  
 cv2.imshow('frame',img)
 cv2.waitKey(0)
